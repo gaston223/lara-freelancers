79860fbdb6528243c5830b821c254805ae10782a
gaston223 <g.coulibalyy@gmail.com>
2020-11-22 20:06:26 +0100

    Install TailwindCss, navbar + logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,12 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Upwork</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <title>Lara-freelancers</title>
 
     </head>
     <body>
       <div class="container mx-auto px-4">
-     
+        @include('partials.navbar')
       @yield('content')
       
       </div>
